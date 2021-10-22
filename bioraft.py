c886fdf203829d2250e90c7a9cd76ff443dd9486
Matt <matthew.morley.ca@gmail.com>
2021-10-22 11:35:25 -0400

    Create bioraft.py

---
+++ bioraft.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+import time, pickle
+
+driver = webdriver.Firefox()
+try:
+    cookies = pickle.load(open("cookies.pkl", "rb"))
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+except Exception:
+    pass
+
+driver.get("https://northeastern.bioraft.com/node/2054541/users")
+
+list = []
+
+# input("Waiting for input...")
+try:
+    element = WebDriverWait(driver, 240).until(
+        EC.presence_of_element_located((By.ID, "edit-userRole"))
+    )
+    print("Starting")
+    pickle.dump( driver.get_cookies() , open("cookies.pkl","wb"))
+
+    for e in list:
+        print(e)
+        ele = driver.find_element_by_id("edit-username")
+        ele.clear()
+        ele.send_keys(str(e))
+        ele = Select(driver.find_element_by_id("edit-userRole"))
+        ele.select_by_index(2)
+        driver.find_element_by_id("edit-lookup").click()
+        time.sleep(1)
+finally:
+    driver.quit()
+
